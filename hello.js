a7cb17a39ece1c74a9af59ccd0322d0da03488ec
Devansh-bansal <devanshbansal42@gmail.com>
2022-11-15 17:33:39 +0000

    Create 5 files

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("hello mf");
+const a = 10;
+const b = 20;
+console.log ("a is " + a);
+if (a > b){
+    console.log("a is greater" + a);
+} else {
+    console.log("b is greater" + b);
+}
+
+    
+
+
